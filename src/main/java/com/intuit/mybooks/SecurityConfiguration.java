bba08841b9ba11644bd92f44e10693d4378f771a
Swapnil Jain <swapniljain240@gmail.com>
2023-03-24 16:56:07 +0530

    In memory Authentication with single user

---
+++ src/main/java/com/intuit/mybooks/SecurityConfiguration.java
@@ -0,0 +1,23 @@
+package com.intuit.mybooks;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class SecurityConfiguration  {
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager(){
+
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("user")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+}
