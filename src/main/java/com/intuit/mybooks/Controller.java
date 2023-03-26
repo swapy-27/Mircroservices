5a6f0115bd758107124b4cead5e582e3e45da7fb
Swapnil Jain <swapniljain240@gmail.com>
2023-03-26 16:32:52 +0530

    learned HTTPSecurity Authorization at API acess level

--- src/main/java/com/intuit/mybooks/Controller.java
+++ src/main/java/com/intuit/mybooks/Controller.java
@@ -8,8 +8,16 @@ public class Controller {
 
 
     @GetMapping(path = "/")
-    public String login(){
+    public String Login(){
         return "<h1>Hello Bro!</h1>";
+    }
+    @GetMapping(path = "/user")
+    public String User(){
+        return "<h1>Hello User!</h1>";
+    }
+    @GetMapping(path = "/admin")
+    public String Admin(){
+        return "<h1>Hello Admin!</h1>";
     }
 
 }
--- src/main/java/com/intuit/mybooks/SecurityConfiguration.java
+++ src/main/java/com/intuit/mybooks/SecurityConfiguration.java
@@ -2,13 +2,17 @@ package com.intuit.mybooks;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfiguration  {
+    //How InMemory Configuration Works.
     @Bean
     public InMemoryUserDetailsManager userDetailsManager(){
 
@@ -17,7 +21,23 @@ public class SecurityConfiguration  {
                 .password("user")
                 .roles("USER")
                 .build();
-
-        return new InMemoryUserDetailsManager(user);
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("admin")
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user,admin);
+    }
+    @Bean
+    //How HTTP Security gets Implemented.
+    SecurityFilterChain web(HttpSecurity http) throws  Exception{
+        http.authorizeHttpRequests(authorize->{
+            authorize
+                    .requestMatchers("/admin").hasRole("ADMIN")
+                    .requestMatchers("/user").hasAnyRole("USER","ADMIN")
+                    .requestMatchers("/").permitAll();
+        })
+                .formLogin();
+        return http.build();
     }
 }
